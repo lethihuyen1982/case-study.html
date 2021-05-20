e404872b4e7d3e47c767c78cc247c23f3a2c16b6
lethihuyen1982 <you@example.com>
2021-05-20 21:10:32 +0700

    them Score of Question

--- question-pool.js
+++ question-pool.js
@@ -1,15 +1,15 @@
 function QuestionPool () {
     let questionPool = [
-        new Question ("Ques1.png","khong khi","k"),
-        new Question ("Ques2.png","ba lo","b"),
-        new Question ("Ques3.png","dong vat","d"),
-        new Question ("Ques4.png","thong qua","t"),
-        new Question ("Ques5.png","nguyen nhan","n"),
-        new Question ("Ques6.png","dat nuoc","d"),
-        new Question ("Ques7.png","nghien cuu","n"),
-        new Question ("Ques8.png","mat troi","m"),
-        new Question ("Ques9.png","nhan dan","n"),
-        new Question ("Ques10.png","nha nuoc","n"),
+        new Question ("Ques1.png","khong khi","k","20"),
+        new Question ("Ques2.png","ba lo","b","10"),
+        new Question ("Ques3.png","dong vat","d","30"),
+        new Question ("Ques4.png","thong qua","t","30"),
+        new Question ("Ques5.png","nguyen nhan","n","20"),
+        new Question ("Ques6.png","dat nuoc","d","10"),
+        new Question ("Ques7.png","nghien cuu","n","10"),
+        new Question ("Ques8.png","mat troi","m","10"),
+        new Question ("Ques9.png","nhan dan","n","20"),
+        new Question ("Ques10.png","nha nuoc","n","20"),
     ]
     
     this.nextQuestion = function() {
